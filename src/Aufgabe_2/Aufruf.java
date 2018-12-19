91be3f86f04b5d373880913f4f8de313c091094a
MingaKoala <44052264+MingaKoala@users.noreply.github.com>
2018-12-19 12:27:58 +0100

    Aufgabe 2 ausgebesser

--- src/Aufgabe_2/Aufruf.java
+++ src/Aufgabe_2/Aufruf.java
@@ -5,7 +5,7 @@ public class Aufruf {
     public static void main(String[] args) {
         Mensch a = new Mensch ("Benedikt", "Oltersdorf", 24);
         a.Status();
-        Student b = new Student("Benedikt", "Oltersdorf", 24, 1810653774, "WB&T");
-        b.Status();
+        a = new Student("Benedikt", "Oltersdorf", 24, 1810653774, "WB&T"); // polymophysmus wenn zb. mensch und student sie selbe methode haben hier a
+        a.Status();
     }
 }
